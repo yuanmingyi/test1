f7648c4f06bddf734232fecec63dcb28cba13ce8
Yuan Mingyi <millan@th122131.ip.tsinghua.edu.cn>
2012-05-27 22:43:49 +0800

    Initial Commit

---
+++ test1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by Yuan Mingyi on 12-5-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test1/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  test1
+//
+//  Created by Yuan Mingyi on 12-5-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
